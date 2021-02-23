2f4f69b2b3534bdce537c6fdb5030fa8c4609193
smohanmeng <smohanmeng@tencent.com>
2021-02-23 16:06:41 +0800

    增加数据建模模块

---
+++ packages/management-home/src/views/schema/data-object/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    数据对象
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'dashboard'
+})
+</script>
