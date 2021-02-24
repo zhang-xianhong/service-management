d9b86adc3fa48a6225a2a3627644ff08cdb0b3b8
smohanmeng <smohanmeng@tencent.com>
2021-02-24 16:52:11 +0800

    增加腾讯规范

--- packages/management-home/src/store/index.ts
+++ packages/management-home/src/store/index.ts
@@ -1,6 +1,6 @@
 import { ModuleTree, createStore } from 'vuex';
 
-const path = require('path');
+import * as path from 'path';
 
 // 创建模块处理上下文，此处为./modules及其下子目录
 const requireModules = require.context('./modules', true, /index\.(ts|js)$/iu);
@@ -10,12 +10,12 @@ const modules: ModuleTree<any> = {};
 requireModules.keys().forEach((filePath: string): void => {
   const modular = requireModules(filePath);
   let name = path.resolve(filePath, '..');
-  name = name.split('/').pop();
+  name = name.split('/').pop() || '';
   modules[name] = {
     namespaced: true,
     ...modular.default
   };
-})
+});
 
 const store = createStore({
   modules: {
