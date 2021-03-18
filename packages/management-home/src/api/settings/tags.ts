5bf44d932fc2e111782972f265a7334cb288b55c
virgilzhao <virgilzhao@tencent.com>
2021-03-18 10:13:42 +0800

    --story=862854145 【CityBase】-标签-前端页面开发 标签管理

--- packages/management-home/src/api/settings/tags.ts
+++ packages/management-home/src/api/settings/tags.ts
@@ -7,3 +7,9 @@ const { settings } = URL;
 export const getAllTags = () => axios.get(getUrl(settings.GET_TAGS_ALL));
 export const listTags: (payload: object) => Promise<SuccessResponse<any>> = (payload: any) =>
   axios.get(getUrl(settings.GET_TAGS), { params: payload });
+export const addTag: (payload: object) => Promise<SuccessResponse<any>> = (payload: any) =>
+  axios.post(getUrl(settings.ADD_TAG), payload);
+export const updateTag: (payload: any) => Promise<SuccessResponse<any>> = ({ id = '', name = '' }) =>
+  axios.post(getUrl(settings.UPDATE_TAG, id), { name });
+export const deleteTags: (payload: any) => Promise<SuccessResponse<any>> = (payload) =>
+  axios.post(getUrl(settings.DELETE_TAG), payload);
--- packages/management-home/src/shared/constant/url/settings.ts
+++ packages/management-home/src/shared/constant/url/settings.ts
@@ -2,7 +2,10 @@
 export default {
   GET_DATA_TYPES: ['/settings/data-types', '/mock/settings/data-types.json'],
   GET_TAGS_ALL: ['settings/tags/all', '/mock/settings/all-tags.json'],
-  GET_TAGS: ['settings/tags', '/mock/settings/all-tags.json'],
+  GET_TAGS: ['settings/tags', '/mock/settings/tag-list.json'],
+  ADD_TAG: ['settings/tags', '/mock/settings/all-tags.json'],
+  UPDATE_TAG: ['settings/tags/_', '/mock/settings/all-tags.json'],
+  DELETE_TAG: ['settings/tags/delete', '/mock/settings/all-tags.json'],
   GET_CATEGORIES: ['settings/categories/tree', '/mock/settings/category-options.json'],
   GET_CLASSIFICATION_LIST: ['/settings/categories/tree', '/mock/settings/classification-list.json'],
   GET_CLASSIFICATION_DETAIL: ['/settings/categories/_', '/mock/settings/classification-detail.json'],
