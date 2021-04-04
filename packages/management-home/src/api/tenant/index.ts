3349ef8c9b92019d6e02a64f3b23baff0fc613ea
virgilzhao <virgilzhao@tencent.com>
2021-04-04 20:58:20 +0800

    --story=863669591 【业务模块】项目详情 用户选择框开发

--- packages/management-home/src/api/tenant/index.ts
+++ packages/management-home/src/api/tenant/index.ts
@@ -3,16 +3,13 @@ import URL from '@/shared/constant/url';
 import { getUrl } from '../utils';
 import { SuccessResponse } from '@/types/response';
 
-<<<<<<< HEAD
 export const getTenantList: (payload?: object) => Promise<SuccessResponse<any>> = (payload: any) =>
   request.get(getUrl(URL.tenant.GET_TENANT_LIST), { params: payload });
 
 export const getTenentDepartment = (payload?: object): Promise<SuccessResponse<any>> =>
   request.get(getUrl(URL.tenant.GET_TENANT_DEPT), { params: payload });
-=======
 export const getTenantDetail: (id: string) => Promise<SuccessResponse<any>> = (id: string) =>
   request.get(getUrl(URL.tenant.GET_TENANT_DETAIL, id));
 
 export const updateTenant: (id: string, payload: any) => Promise<SuccessResponse<any>> = (id: string, payload: any) =>
   request.post(getUrl(URL.tenant.UPDATE_TENT, id), payload);
->>>>>>> 20ee28ca8acb68f84c3e1225fae1364100fa7748
