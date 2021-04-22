0b7a3caff85b5061d7a4c528cc0e0151f417e946
aidentao <aidentao@tencent.com>
2021-04-22 15:06:14 +0800

    fix： 设计走查问题整改

--- packages/management-home/src/views/project-management/ProjectList.vue
+++ packages/management-home/src/views/project-management/ProjectList.vue
@@ -33,74 +33,74 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="pageInfo.total"
     ></packaged-pagination>
+    <el-dialog v-model="addDialogVisible" title="新建项目" width="500px" @close="closeDialog" destroy-on-close>
+      <div class="add-project-dialog">
+        <el-form :model="projectDetail">
+          <el-form-item
+            label="项目名称"
+            :label-width="labelWidth"
+            prop="name"
+            :rules="[{ required: true, message: '请输入英文名称', trigger: 'blur' }]"
+          >
+            <el-input v-model="projectDetail.name" @blur="checkEnglishName"> </el-input>
+          </el-form-item>
+          <el-form-item
+            label="项目描述"
+            :label-width="labelWidth"
+            prop="name"
+            :rules="[{ required: true, message: '请输入项目描述', trigger: 'blur' }]"
+          >
+            <el-input v-model="projectDetail.description"> </el-input>
+          </el-form-item>
+          <el-form-item
+            label="代码模板"
+            :label-width="labelWidth"
+            :rules="[{ required: true, message: '请选择代码模板', trigger: 'blur' }]"
+          >
+            <el-select v-model="projectDetail.templateId" placeholder="请选择代码模板" default-first-option>
+              <el-option
+                v-for="(item, index) in codeTemplateList"
+                :key="index"
+                :label="item.name"
+                :value="item.id"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="负责人" :label-width="labelWidth">
+            <fetch-owners-select @get-owners="setOwner"></fetch-owners-select>
+          </el-form-item>
+          <el-form-item
+            label="项目级别"
+            :label-width="labelWidth"
+            :rules="[{ required: true, message: '请输入项目级别', trigger: 'blur' }]"
+          >
+            <el-radio v-model="projectDetail.level" :label="1">通用级</el-radio>
+            <el-radio v-model="projectDetail.level" :label="2">行业级</el-radio>
+            <el-radio v-model="projectDetail.level" :label="3">租户级</el-radio>
+          </el-form-item>
+          <el-form-item
+            label="许可类型"
+            :label-width="labelWidth"
+            :rules="[{ required: true, message: '请输入项目级别', trigger: 'blur' }]"
+          >
+            <el-radio v-model="projectDetail.license" :label="0">永久</el-radio>
+            <el-radio v-model="projectDetail.license" :label="1">租用</el-radio>
+          </el-form-item>
+          <el-form-item label="项目状态" :label-width="labelWidth">
+            <el-radio v-model="projectDetail.status" :label="1">启用</el-radio>
+            <el-radio v-model="projectDetail.status" :label="0">禁用</el-radio>
+          </el-form-item>
+          <el-form-item label="项目简介" :label-width="labelWidth">
+            <el-input v-model="projectDetail.remark" type="textarea" :rows="5"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="project-list-dialog-footer">
+        <el-button type="primary" @click="submitProjectDetail">确定</el-button>
+        <el-button @click="closeDialog">取消</el-button>
+      </div>
+    </el-dialog>
   </div>
-  <el-dialog v-model="addDialogVisible" title="新建项目" width="500px" @close="closeDialog" destroy-on-close>
-    <div class="add-project-dialog">
-      <el-form :model="projectDetail">
-        <el-form-item
-          label="项目名称"
-          :label-width="labelWidth"
-          prop="name"
-          :rules="[{ required: true, message: '请输入英文名称', trigger: 'blur' }]"
-        >
-          <el-input v-model="projectDetail.name" @blur="checkEnglishName"> </el-input>
-        </el-form-item>
-        <el-form-item
-          label="项目描述"
-          :label-width="labelWidth"
-          prop="name"
-          :rules="[{ required: true, message: '请输入项目描述', trigger: 'blur' }]"
-        >
-          <el-input v-model="projectDetail.description"> </el-input>
-        </el-form-item>
-        <el-form-item
-          label="代码模板"
-          :label-width="labelWidth"
-          :rules="[{ required: true, message: '请选择代码模板', trigger: 'blur' }]"
-        >
-          <el-select v-model="projectDetail.templateId" placeholder="请选择代码模板" default-first-option>
-            <el-option
-              v-for="(item, index) in codeTemplateList"
-              :key="index"
-              :label="item.name"
-              :value="item.id"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="负责人" :label-width="labelWidth">
-          <fetch-owners-select @get-owners="setOwner"></fetch-owners-select>
-        </el-form-item>
-        <el-form-item
-          label="项目级别"
-          :label-width="labelWidth"
-          :rules="[{ required: true, message: '请输入项目级别', trigger: 'blur' }]"
-        >
-          <el-radio v-model="projectDetail.level" :label="1">通用级</el-radio>
-          <el-radio v-model="projectDetail.level" :label="2">行业级</el-radio>
-          <el-radio v-model="projectDetail.level" :label="3">租户级</el-radio>
-        </el-form-item>
-        <el-form-item
-          label="许可类型"
-          :label-width="labelWidth"
-          :rules="[{ required: true, message: '请输入项目级别', trigger: 'blur' }]"
-        >
-          <el-radio v-model="projectDetail.license" :label="0">永久</el-radio>
-          <el-radio v-model="projectDetail.license" :label="1">租用</el-radio>
-        </el-form-item>
-        <el-form-item label="项目状态" :label-width="labelWidth">
-          <el-radio v-model="projectDetail.status" :label="1">启用</el-radio>
-          <el-radio v-model="projectDetail.status" :label="0">禁用</el-radio>
-        </el-form-item>
-        <el-form-item label="项目简介" :label-width="labelWidth">
-          <el-input v-model="projectDetail.remark" type="textarea" :rows="5"></el-input>
-        </el-form-item>
-      </el-form>
-    </div>
-    <div class="project-list-dialog-footer">
-      <el-button type="primary" @click="submitProjectDetail">确定</el-button>
-      <el-button @click="closeDialog">取消</el-button>
-    </div>
-  </el-dialog>
 </template>
 
 <script lang="ts">
