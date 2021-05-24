4e7e087abf3aa222db1ebcd6272748d012ea80a6
kyleezhang <kyleezhang@tencent.com>
2021-05-24 16:15:53 +0800

    feat: '--task=登录接口调整

--- packages/management-home/src/shared/constant/url/auth.ts
+++ packages/management-home/src/shared/constant/url/auth.ts
@@ -5,5 +5,7 @@ export default {
   GET_HEALTH: ['/health', '/mock/auth/health.json'],
   LOGOUT: ['/logout'],
   GET_CAPTCHA: ['/auth/captcha', 'mock/auth/captcha.json'],
-  LOGIN: ['auth/login', 'mock/auth/login.json'],
+  LOGIN: ['/login', 'mock/auth/login.json'],
+  CODE: ['auth/login/code', 'mock/auth/code.json'],
+  VERIFY_CAPTCHA: ['/auth/captcha/verify', 'mock/auth/verify.json'],
 };
