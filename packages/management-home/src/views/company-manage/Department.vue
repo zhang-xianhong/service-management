0aabfd3fd199ee8cfe0c1f38b51a1ca2d28adfdf
jaggerguo <jaggerguo@tencent.com>
2021-05-27 11:29:42 +0800

    fix: 跟部门添加子部门问题

--- packages/management-home/src/views/company-manage/Department.vue
+++ packages/management-home/src/views/company-manage/Department.vue
@@ -6,10 +6,9 @@
           type="primary"
           style="width: 90px"
           @click="handleAddDept"
-          :disabled="currentNodeData.id === 0 || !isSel"
-          >添加子部门</el-button
-        >
-        <el-button @click="handleDel" :disabled="!isSel">删除</el-button>
+          :disabled="!isSel"
+        >添加子部门</el-button>
+        <el-button @click="handleDel" :disabled="currentNodeData.id === 0 ? true : !isSel">删除</el-button>
       </el-col>
       <el-col :offset="10" :span="4" style="text-align: right">
         <el-input
@@ -41,7 +40,9 @@
                 <div>
                   <svg-icon v-if="data._children" icon-name="folder" icon-class="tree-node-folder"></svg-icon>
                   <svg-icon v-else icon-name="person" icon-class="tree-node-folder"></svg-icon>
-                  <span style="z-index: 1; background: transparent; margin-right: 5px">{{ data.name }}</span>
+                  <span
+                    style="z-index: 1; background: transparent; margin-right: 5px"
+                  >{{ data.name }}</span>
                   <el-dropdown v-if="data._children && data.id !== 0">
                     <span class="el-dropdown-link">
                       <i class="el-icon-more" style="transform: rotate(90deg)"></i>
@@ -52,8 +53,7 @@
                         <el-dropdown-item
                           @click="handleUpMove(data)"
                           v-if="data.id !== 0 && data.parent && data.parent.id !== 0"
-                          >上移一层</el-dropdown-item
-                        >
+                        >上移一层</el-dropdown-item>
                       </el-dropdown-menu>
                     </template>
                   </el-dropdown>
@@ -243,7 +243,6 @@ export default defineComponent({
     });
     // 编辑的数据
     const editFormData = ref();
-
     // 所有的人员
     const allUsers = ref([]);
     // 当前node节点下的人
@@ -492,7 +491,7 @@ export default defineComponent({
     function nodeClickHandle(data: any, node: any): void {
       console.log('当前节点数据', data);
       // 首节点不做选中
-      treeData.isSel = data.id !== 0;
+      treeData.isSel = true;
       treeData.currentNodeData = data;
       treeData.currentNode = node;
       const { page, pageSize } = tableData.searchProps;
