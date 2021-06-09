614e84eb58e644a7cb1789f180246b05f4b44288
jaggerguo <jaggerguo@tencent.com>
2021-06-09 10:41:03 +0800

    fix:--bug=88590175 【超级管理平台验证环境----租户列表】租户列表，冻结租户的时候，没有弹窗二次提示

--- packages/management-admin/src/views/tenant-management/Index.vue
+++ packages/management-admin/src/views/tenant-management/Index.vue
@@ -42,7 +42,7 @@
           <el-button
             type="text"
             v-if="scope.row.status === statusEnum.START"
-            @click="onFreeze(scope.row.id)"
+            @click="onFreeze(scope.row)"
           >冻结</el-button>
           <el-button
             type="text"
@@ -71,6 +71,7 @@
 
 <script lang="ts">
 import { reactive, toRefs, getCurrentInstance } from 'vue';
+import { ElMessageBox, ElMessage } from 'element-plus';
 import { getTenantList, deleteTenant, freezeTenant, enableTenant } from '@/api/tenant';
 import { debounce } from 'lodash';
 import { useRouter } from 'vue-router';
@@ -209,15 +210,28 @@ export default {
     };
 
     // 租户冻结
-    const onFreeze = async (id: string) => {
-      const { code } = await freezeTenant(id);
-      if (code === 0) {
-        (instance as any).proxy.$message({
-          type: 'success',
-          message: '冻结成功',
+    const onFreeze = async (data: any) => {
+      ElMessageBox.confirm(`是否冻结【${data.name}】租户?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(async () => {
+          const { code } = await freezeTenant(data.id);
+          if (code === 0) {
+            (instance as any).proxy.$message({
+              type: 'success',
+              message: '冻结成功',
+            });
+            getTableData();
+          }
+        })
+        .catch(() => {
+          ElMessage({
+            type: 'info',
+            message: '冻结失败',
+          });
         });
-        getTableData();
-      }
     };
 
     // 租户启动
