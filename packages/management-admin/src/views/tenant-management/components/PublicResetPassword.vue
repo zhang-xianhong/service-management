684a4a3c1cb8db782e628bc84335db1b07cd4b88
jaggerguo <jaggerguo@tencent.com>
2021-06-18 14:25:40 +0800

    fix; --bug=88947087 【超级管理平台验证环境----项目列表】重置密码输入框，能输入除!@#$%^&特殊字符之外的，例如你能输入+

--- packages/management-admin/src/views/tenant-management/components/PublicResetPassword.vue
+++ packages/management-admin/src/views/tenant-management/components/PublicResetPassword.vue
@@ -48,7 +48,7 @@ interface ResetFormState {
 }
 // 自定义密码校验  长度在 8 到 16 个字符,只能输入大小写字母、数字、特殊字符（(!@#$%^&),至少1个大写字母，1个小写字母
 function checkPasswd(passwd: string): boolean {
-  const szReg = /^(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d（!@#$%^&)]{8,16}/;
+  const szReg = /^(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d（!@#$%^&)]{8,16}$/;
   return szReg.test(passwd);
 }
 const HELP_MSG = '长度在 8 到 16 个字符,只能输入大小写字母、数字、特殊字符（(!@#$%^&),至少1个大写字母，1个小写字母';
