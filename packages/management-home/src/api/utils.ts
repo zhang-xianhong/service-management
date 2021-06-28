3f0b439afb113c68aefb8076bbaa05cf32b527a2
aidentao <aidentao@tencent.com>
2021-06-28 19:52:08 +0800

    fix：lint code

--- packages/management-home/src/api/utils.ts
+++ packages/management-home/src/api/utils.ts
@@ -1,4 +1,4 @@
-export const isProduction = process.env.NODE_ENV === 'production';
+export const isProduction = process.env.NODE_ENV !== 'production';
 
 const useMock = Number(process.env.VUE_APP_USE_MOCK) === 1;
 export const getUrl = (urls: string[], params = ''): string => {
