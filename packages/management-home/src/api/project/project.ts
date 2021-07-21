aa79a64acce29b4a2f22a37c3fa27e3e347b93a8
wxiaopwang <wxiaopwang@tencent.com>
2021-07-21 18:26:31 +0800

    feat: '--story=866488395 【产品需求】角色权限

--- packages/management-home/src/api/project/project.ts
+++ packages/management-home/src/api/project/project.ts
@@ -20,7 +20,7 @@ export const updateProject = (projectId: number, payload: any): Promise<SuccessR
 export const getMemberList = (payload: Record<string, string>): Promise<SuccessResponse<any>> =>
   axios.get(getUrl(project.GET_MEMBER_LIST, payload.projectId));
 
-  export const getRoleList = (payload: Record<string, string>): Promise<SuccessResponse<any>> =>
+export const getRoleList = (payload: Record<string, string>): Promise<SuccessResponse<any>> =>
   axios.get(getUrl(project.GET_ROLE_LIST, payload.projectId));
 
 export const updateMembers = (payload: any): Promise<SuccessResponse<any>> =>
