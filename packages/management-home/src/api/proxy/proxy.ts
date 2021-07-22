1b2c6ef596abdd0e7c8a72b83ad9b8eed6208766
wxiaopwang <wxiaopwang@tencent.com>
2021-07-22 15:45:43 +0800

    feat: '--story=866488395 【产品需求】角色权限

--- packages/management-home/src/api/proxy/proxy.ts
+++ packages/management-home/src/api/proxy/proxy.ts
@@ -3,8 +3,8 @@ import { SuccessResponse } from '@/types/response';
 import request from '@/utils/request';
 import { getUrl } from '../utils';
 export const apiProxy = (module: string, action: string[], data: any): Promise<SuccessResponse<any>> => {
-    const url = !getUrl(proxy.PROXY).includes('mock')
-        ? `${getUrl(proxy.PROXY)}?module=${module}&action=${getUrl(action)}`
-        : getUrl(action);
-    return request.post(url, data);
+  const url = !getUrl(proxy.PROXY).includes('mock')
+    ? `${getUrl(proxy.PROXY)}?module=${module}&action=${getUrl(action)}`
+    : getUrl(action);
+  return request.post(url, data);
 };
